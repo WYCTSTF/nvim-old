529aa828abf58fb9a4319b62f510fc02159e7974
WYCTSTF <996098413@qq.com>
2023-05-17 20:00:09 +0800

    configuration file

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 require("basic")
 require("plugins")
+require("keybindings")
+require("colorscheme")
+
+require("plugin-config/lualine")
+require("plugin-config/nvim-tree")
+require("plugin-config/bufferline")
+require("plugin-config/nvim-treesitter")
+
+-- LSP
+require("mason").setup()
+require("lsp/setup")
