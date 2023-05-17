c8be865113888441119b07e13caefa131100f104
WYCTSTF <996098413@qq.com>
2023-05-17 20:01:07 +0800

    键位绑定

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -63,3 +63,23 @@ map("n", "<leader>bc", ":BufferLinePickClose<CR>", opt)
 map("n", "<C-p>", ":Telescope find_files<CR>", opt)
 -- 全局搜索
 map("n", "<C-f>", ":Telescope live_grep<CR>", opt)
+
+-- LSP
+-- rename
+map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opt)
+-- code action
+map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opt)
+-- go xx
+map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
+map("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opt)
+map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opt)
+map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
+map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opt)
+-- diagnostic
+map("n", "gp", "<cmd>lua vim.diagnostic.open_float()<CR>", opt)
+map("n", "gk", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opt)
+map("n", "gj", "<cmd>lua vim.diagnostic.goto_next()<CR>", opt)
+map("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opt)
+
+map("n", "<F5>", ":T clang++ -O2 -std=c++17 -fsanitize=address %:p -o %:p:r && %:p:r<CR>", opt)
+map("n", "<F6>", ":T clang++ -O2 -std=c++17 -fsanitize=address %:p -o %:p:r && %:p:r < %:p:h/in <CR>", opt)
