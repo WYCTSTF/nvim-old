bc4ddb3ca677e5070933be1531520c8f60a2a1c9
WYCTSTF <996098413@qq.com>
2023-05-17 14:19:38 +0800

    a new configuration

--- lua/colorScheme.lua
+++
@@ -1,9 +0,0 @@
-local colorScheme = "gruvbox"
-local status, _ = pcall(vim.cmd, "colorscheme "..colorScheme)
-local status_1, _1 = pcall(vim.cmd, "set background=dark")
-local status_2, _2 = pcall(vim.cmd, "highlight Normal guibg=NONE ctermbg=None")
--- klvim.cmd(colorscheme material:)
-if not status then
-  vim.notify(colorScheme.." not Found!")
-  return
-end
