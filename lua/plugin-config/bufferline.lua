a82a6c050cea30d4c0609a40183f05aa5510a3a4
WYCTSTF <996098413@qq.com>
2023-05-17 20:01:20 +0800

    标签管理插件

---
+++ lua/plugin-config/bufferline.lua
@@ -0,0 +1,17 @@
+require("bufferline").setup{
+	options = {
+    -- 关闭 Tab 的命令，这里使用 moll/vim-bbye 的 :Bdelete 命令
+    close_command = "Bdelete! %d",
+    right_mouse_command = "Bdelete! %d",
+    -- 侧边栏配置
+    -- 左侧让出 nvim-tree 的位置，显示文字 File Explorer
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        highlight = "Directory",
+        text_align = "left",
+      },
+    },
+  },
+}
